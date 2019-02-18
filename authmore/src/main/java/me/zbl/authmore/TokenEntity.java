970992e14c1579beef7a94120b0b10ac711d30e5
摆码王子 <1146556298@qq.com>
2019-02-18 21:07:24 +0800

    token entity

---
+++ authmore/src/main/java/me/zbl/authmore/TokenEntity.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 JamesZBL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package me.zbl.authmore;
+
+/**
+ * @author JamesZBL
+ * @since 2019-02-18
+ */
+public class TokenEntity {
+
+    private String access_token;
+    private long expires_in;
+    private String refresh_token;
+    private String scope;
+
+    public TokenEntity(String access_token, long expires_in, String refresh_token, String scope) {
+        this.access_token = access_token;
+        this.expires_in = expires_in;
+        this.refresh_token = refresh_token;
+        this.scope = scope;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public long getExpires_in() {
+        return expires_in;
+    }
+
+    public void setExpires_in(long expires_in) {
+        this.expires_in = expires_in;
+    }
+
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+
+    public void setRefresh_token(String refresh_token) {
+        this.refresh_token = refresh_token;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+}
