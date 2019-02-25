9b50616fe5041ab450b34cb3d3205301de97350e
摆码王子 <1146556298@qq.com>
2019-02-25 21:32:10 +0800

    make cartapp supporting all type

--- auth/src/main/java/me/zbl/reactivesecurity/auth/client/ClientInitializer.java
+++ auth/src/main/java/me/zbl/reactivesecurity/auth/client/ClientInitializer.java
@@ -34,7 +34,8 @@ public class ClientInitializer implements SmartInitializingSingleton {
 
     @Override
     public void afterSingletonsInstantiated() {
-        ClientDetails client = new ClientDetails("cartapp", "authorization_code", "cart:read",
+        ClientDetails client = new ClientDetails("cartapp",
+                "authorization_code,password,implicit,client_credentials", "cart:read",
                 "{pbkdf2}30d47c8ef17066e65750bb6469b951dbaf8b40d4cf4b421490ffff92da00804700c8b8fb92cc9ce0",
                 "http://localhost:8084/login", 999999);
 
