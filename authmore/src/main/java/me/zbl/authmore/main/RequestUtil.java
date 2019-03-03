9787da908c3c4947d47fcf0f1c7ade3f4cbbcc60
摆码王子 <1146556298@qq.com>
2019-03-03 23:21:20 +0800

    update copyright with real name

--- authmore/src/main/java/me/zbl/authmore/main/RequestUtil.java
+++ authmore/src/main/java/me/zbl/authmore/main/RequestUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 JamesZBL
+ * Copyright 2019 ZHENG BAO LE
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
