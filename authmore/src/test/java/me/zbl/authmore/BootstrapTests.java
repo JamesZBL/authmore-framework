d11c22b38c043f49a457854d5c20ae1fa480883d
摆码王子 <1146556298@qq.com>
2019-03-05 18:17:15 +0800

    bootstrap test

--- authmore/src/test/java/me/zbl/authmore/BootstrapTests.java
+++ authmore/src/test/java/me/zbl/authmore/BootstrapTests.java
@@ -15,18 +15,18 @@
  */
 package me.zbl.authmore;
 
+import me.zbl.authmore.main.Bootstrap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {Bootstrap.class})
 public class BootstrapTests {
 
     @Test
-    public void contextLoads() {
-    }
+    public void contextLoads() {}
 
 }
 
