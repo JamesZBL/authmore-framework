7b4d0df8966cc80eb576d43dae51f4d2e24afdf2
摆码王子 <1146556298@qq.com>
2019-04-23 16:47:54 +0800

    ssh-agent

--- deploy.remote.preview.sh
+++ deploy.remote.preview.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 key_file=~/.ssh/key
 echo $SSH_PRIVATE_KEY > $key_file
+chmod 600 $key_file
 eval `ssh-agent -s`
 ssh-add $key_file
-chmod 600 $key_file
 ssh -o StrictHostKeyChecking=no root@$SERVER_HOST -i $key_file 'docker stack deploy --with-registry-auth \
     -c repo/authmore.preview.yml authmore'
