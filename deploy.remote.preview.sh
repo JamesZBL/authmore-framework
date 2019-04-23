38771ae829b53c302068658bae07c7076cd63ef6
摆码王子 <1146556298@qq.com>
2019-04-23 14:33:26 +0800

    limit permission for private key

--- deploy.remote.preview.sh
+++ deploy.remote.preview.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo $SSH_PRIVATE_KEY > ~/.ssh/key
+key_file="~/.ssh/key"
+echo $SSH_PRIVATE_KEY > $key_file
+chmod 600 $key_file
 ssh -o StrictHostKeyChecking=no root@$SERVER_HOST -i ~/.ssh/key 'docker stack deploy --with-registry-auth \
     -c repo/authmore.preview.yml authmore'
