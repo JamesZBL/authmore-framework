09bea00d16f3c7fb417d4286a5467309f4c2ac58
摆码王子 <1146556298@qq.com>
2019-05-14 23:20:11 +0800

    rename modules in scripts

--- build.preview.sh
+++ build.preview.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 prefix='registry.cn-beijing.aliyuncs.com/letec/'
 modules=(auth authmore)
+dirs=(authmore-admin authmore-platform)
+i=1
 for m in "${modules[@]}"; do
     img_name=$prefix$m
-    ( cd $m && docker build -f Dockerfile.test -t $img_name . && docker push $img_name )
+    dir=${dirs[i]}
+    ( cd $dir && docker build -f Dockerfile.test -t $img_name . && docker push $img_name )
+    i=$((i+1))
 done
