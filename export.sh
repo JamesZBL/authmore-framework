70d868d1d33453c2ad90a43420148270bff60a68
摆码王子 <1146556298@qq.com>
2019-05-16 17:56:14 +0800

    only export zip

--- export.sh
+++ export.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-mkdir -p archive
-fileNamePrefix="authmore-framework-"
-for i in master dev; do
-    for t in tar.gz zip; do
-        git archive --format $t --output archive/$fileNamePrefix$i.$t $i
+home="archive"
+mkdir -p $home
+remote="git@gitee.com:zbl1996"
+for i in master; do
+    for t in zip; do
+        git archive --format $t --output archive/authmore-framework-$i.$t --remote=$remote/authmore.git $i
+        git archive --format $t --output archive/authmore-ui-$i.$t --remote=$remote/authmore-ui.git $i
+        (cd $home && zip authmore.zip authmore-framework-$i.$t authmore-ui-$i.$t)
     done
 done
 echo Export finished.
