7a075fb5914bd66f78d4af6225cae57c992e430a
摆码王子 <1146556298@qq.com>
2019-05-16 18:37:48 +0800

    combine ui and framework to one zip file

--- export.sh
+++ export.sh
@@ -4,9 +4,16 @@ mkdir -p $home
 remote="git@gitee.com:zbl1996"
 for i in master; do
     for t in zip; do
-        git archive --format $t --output archive/authmore-framework-$i.$t --remote=$remote/authmore.git $i
-        git archive --format $t --output archive/authmore-ui-$i.$t --remote=$remote/authmore-ui.git $i
-        (cd $home && zip authmore.zip authmore-framework-$i.$t authmore-ui-$i.$t)
+        git archive --prefix=authmore-ui/ --format=$t --output archive/authmore-ui-$i.$t --remote=$remote/authmore-ui.git $i
+        git archive --prefix=authmore-framework/ --format=$t --output archive/authmore-framework-$i.$t --remote=$remote/authmore.git $i
+        cd $home
+        mkdir tmp
+        for f in authmore-*-$i.$t; do
+            unzip -d tmp -o -u $f && rm $f
+        done
+        (cd tmp && zip -o authmore.$t -r authmore.$t .)
+        mv tmp/authmore.$t .
+        rm -r tmp
     done
 done
 echo Export finished.
