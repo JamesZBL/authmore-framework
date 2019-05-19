8b1a11ae319ec323337e416aec64af24e9dd8295
摆码王子 <1146556298@qq.com>
2019-05-19 15:46:10 +0800

    update remote deploy script

--- deploy.remote.preview.sh
+++ deploy.remote.preview.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh root@$SERVER_HOST '(cd repo/authmore && git pull && ./gradlew build --scan -s -x test dockerBuildImage) \
+ssh root@$SERVER_HOST '(cd repo/authmore && git pull -f && ./gradlew build --scan -s -x test dockerBuildImage dockerPushImage) \
     && docker stack deploy --with-registry-auth -c repo/authmore.preview.yml authmore'
