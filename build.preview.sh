51768656e227a41ce7390c65709b2b6dc6bbe38e
摆码王子 <1146556298@qq.com>
2019-05-19 14:00:59 +0800

    refactor docker

--- build.preview.sh
+++ build.preview.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-prefix='registry.cn-beijing.aliyuncs.com/letec/'
-modules=(auth authmore)
-dirs=(authmore-admin authmore-platform)
-i=1
-for m in "${modules[@]}"; do
-    img_name=$prefix$m
-    dir=${dirs[i]}
-    ( cd $dir && docker build -f Dockerfile.test -t $img_name . && docker push $img_name )
-    i=$((i+1))
-done
+#prefix='registry.cn-beijing.aliyuncs.com/letec/'
+#modules=(authmore-admin authmore-platform)
+#i=1
+#for m in "${modules[@]}"; do
+#    img_name=$prefix$m
+#    ( cd $m && docker build -f Dockerfile.test -t $img_name . && docker push $img_name )
+#done
+./gradlew dockerBuildImage dockerPushImage
